d6ea08ad5dfae408e9644960ff7f9935fde5a2ef
thiagorigonatti <25761610+thiagorigonatti@users.noreply.github.com>
2024-02-15 07:33:50 -0300

    Fix adding trademark™ character when editing title; Added a 3rd offset for replacing game name in games tab at xbox360 dash

--- xbox360-retitler.js
+++ xbox360-retitler.js
@@ -1,23 +1,54 @@
 class XBOX360Retitler {
   static readTitle(array) {
     let title = "";
+
     for (let i = 0x412; array[i] !== 0x00; i += 2) {
+
+      if(array[i] === 0x22 && array[i - 1] === 0x21) {
+        title += "™";
+        continue;
+      }
+
       title += String.fromCharCode(array[i]);
+
     }
+
     return title;
   }
 
   static changeTitle(array, title) {
+
     let i = 0x412;
+
+    let check3rd = array[i + 0x800] !== 0x00;
+
     for (let j = 0; j < title.length; j++) {
-      array[i] = title.charCodeAt(j);
-      array[i + 0x1280] = title.charCodeAt(j);
+
+      if(title.charCodeAt(j) === 0x2122) {
+        array[i - 1] = 0x21;
+        array[i] = 0x22;
+
+        array[i + 0x1280 - 1] = 0x21;
+        array[i + 0x1280] = 0x22;
+
+        if(check3rd) {
+          array[i + 0x800 - 1] = 0x21;
+          array[i + 0x800] = 0x22;
+        }
+      }
+
+      else {
+        array[i] = title.charCodeAt(j);
+        array[i + 0x1280] = title.charCodeAt(j);
+
+        if(check3rd) {
+          array[i + 0x800] = title.charCodeAt(j);
+        }
+      }
+
       i += 2;
     }
-    for (; array[i] !== 0x00; i += 2) {
-      array[i] = 0x00;
-      array[i + 0x1280] = 0x00;
-    }
+
   }
 
   static readHash(array) {
